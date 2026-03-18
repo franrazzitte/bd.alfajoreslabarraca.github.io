edcd766bca44b5213854161b9659034710066575
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2026-03-17 22:39:56 -0300

    Update & Add files

--- js/eventos.js
+++ js/eventos.js
@@ -16,6 +16,20 @@ const proximosEventos = [
     iframe: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.9638795660626!2d-58.4209327!3d-34.5797805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb59cd6452553%3A0x6b521307d135059a!2sLa%20Rural!5e0!3m2!1ses-419!2sar!4v1772509628887!5m2!1ses-419!2sar'
   },
   {
+    titulo: '¡Primer Festival del Alfajor 2026!',
+    tituloAlter: 'Festival del Alfajor',
+    diaI: 22,
+    diaF: 22,
+    mes: 3,
+    año: 2026,
+    horaI: '12',
+    horaF: '21',
+    ubicacion: 'Plaza Balcarce, Ciudad de Suipacha',
+    desc: '¡Vamos a Suipacha! Este 22 de marzo de 12 a 21 horas podrás encontrarnos en la primera edición del Festival del Alfajor en la Ciudad de Suipacha. En este evento podrás probar varios de nuestros sabores más exquisitos. Además, habrán shows en vivo, master classes, y mucho más. ¿Te lo vas a perder?',
+    imagenes: 'festival-del-alfajor2026',
+    iframe: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.9638795660626!2d-58.4209327!3d-34.5797805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb59cd6452553%3A0x6b521307d135059a!2sLa%20Rural!5e0!3m2!1ses-419!2sar!4v1772509628887!5m2!1ses-419!2sar'
+  },
+  {
     titulo: '¡Primer Salón del Alfajor 2026!',
     tituloAlter: 'Salón del Alfajor',
     diaI: 17,
