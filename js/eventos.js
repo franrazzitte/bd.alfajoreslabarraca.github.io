d4c84559bf7f04101419326df535acb3b206520f
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2026-04-24 22:30:19 -0300

    Update eventos.js

--- js/eventos.js
+++ js/eventos.js
@@ -42,5 +42,19 @@ const proximosEventos = [
     desc: '¡Estamos orgullosos de anunciar que vamos a formar parte de la primera edición del Salón del Alfajor! Esta se realizará en el histórico Predio Floral de Escobar los días 17, 18 y 19 de abril, de 12 a 20hrs. ¡Te esperamos!',
     imagenes: 'salon-del-alfajor2026',
     iframe: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13177.717458821338!2d-58.79292774738921!3d-34.33954904850781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bb61a4d84c286b%3A0x807fdacdeaa64396!2sMateo%20Gelves%201050%2C%20B1625%20Bel%C3%A9n%20de%20Escobar%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1773120792564!5m2!1ses-419!2sar'
+  },
+  {
+    titulo: "Feria de Campo Ranch's Market",
+    tituloAlter: "Ranch's Market",
+    diaI: 26,
+    diaF: 26,
+    mes: 4,
+    año: 2026,
+    horaI: '13',
+    horaF: '19',
+    ubicacion: 'Colectora AU Ezeiza Cañuelas 58.5KM',
+    desc: "Este 26 de abril de 13 a 19hrs vamos a estar en la Feria de Campo Ranch's Market. Nos podrás encontrar sobre la colectora de la Autopista Ezeiza-Cañuelas. La entrada es libre y gratuita. ¡Los esperamos!",
+    imagenes: '',
+    iframe: 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2987.4330852458497!2d-58.71715892424247!3d-34.999120572815144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzTCsDU5JzU2LjgiUyA1OMKwNDInNTIuNSJX!5e1!3m2!1ses-419!2sar!4v1777079131429!5m2!1ses-419!2sar'
   }
 ];
